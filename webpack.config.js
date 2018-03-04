89cce136f5673373e2531422cb16399070b9006d
Jules Bonnard <julesbonnard@gmail.com>
2018-03-04 01:33:05 +0100

    add building and config files

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+var moduleConfig = {
+  rules: [
+    {
+      enforce: 'pre',
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'eslint-loader'
+      }
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }
+  ]
+}
+
+var serverConfig = {
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'afpnews.js',
+    library: 'afpnews',
+    libraryTarget: 'umd'
+  },
+  module: moduleConfig
+};
+
+var clientConfig = {
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'afpnews.browser.js'
+  },
+  module: moduleConfig
+};
+
+module.exports = [ serverConfig, clientConfig ];
