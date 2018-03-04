ef5b9588f93a13d29831a3dc09dc444a63607f86
Jules Bonnard <julesbonnard@gmail.com>
2018-03-04 18:08:13 +0100

    add some unit testing

--- test/afpNews.spec.js
+++ test/afpNews.spec.js
@@ -1,34 +1,140 @@
-const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
-chai.should()
+require('dotenv').config();
 
-const { assert, expect } = chai
-const AfpNews = require('../')
+const chai = require('chai');
+const expect = chai.expect;
+
+const AfpNews = require('../');
+
+const apiKey = process.env.AFPNEWS_API_KEY;
+const username = process.env.AFPNEWS_USERNAME;
+const password = process.env.AFPNEWS_PASSWORD;
 
 describe('AFP News', function() {
   describe('Initialization', function() {
     it('should return true when afpNews is instance of AfpNews', function() {
-      const afpNews = new AfpNews()
-      assert.equal(afpNews instanceof AfpNews, true);
+      const afpNews = new AfpNews();
+      expect(afpNews instanceof AfpNews).to.be.true;
     });
     it('should reset token on init', function() {
-      const afpNews = new AfpNews()
-      assert.deepEqual(afpNews.token, { accessToken: null, refreshToken: null, tokenExpires: null });
+      const afpNews = new AfpNews();
+      const { accessToken, refreshToken, tokenExpires } = afpNews.token;
+      expect(accessToken).to.be.null;
+      expect(refreshToken).to.be.null;
+      expect(tokenExpires).to.be.null;
     });
-    it('should allow to change base url', function() {
-      const afpNews = new AfpNews({ baseUrl: 'http://customurl' })
-      assert.equal(afpNews.baseUrl, 'http://customurl');
+    it('should allow to change base url in constructor', function() {
+      const afpNews = new AfpNews({ baseUrl: 'http://customurl' });
+      expect(afpNews.baseUrl).to.be.equal('http://customurl');
     });
     it('should accept an apiKey', function() {
-      const afpNews = new AfpNews({ apiKey: 'TEST' })
-      assert.equal(afpNews._apiKey, 'TEST');
+      const afpNews = new AfpNews({ apiKey: 'TEST' });
+      expect(afpNews._apiKey).to.be.equal('TEST');
     });
   });
   describe('Authentication', function() {
-    it('should return a token when called without api key', function() {
-      const afpNews = new AfpNews()
-      return afpNews.authenticate().should.eventually.fulfilled
+    it('should return an anonymous token when called without api key', async function() {
+      const afpNews = new AfpNews();
+      const token = await afpNews.authenticate();
+      expect(token.accessToken).to.be.a('string');
+      expect(token.refreshToken).to.be.a('string');
+      expect(token.tokenExpires).to.be.a('number');
+      expect(token).to.deep.equal(afpNews.token);
+    });
+    it('should throw if called with api key but without credentials', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      try {
+        await afpNews.authenticate();
+      } catch (e) {
+        expect(e).to.be.an('error');
+      }
+    });
+    it('should throw if called with credentials but without api key', async function() {
+      const afpNews = new AfpNews();
+      try {
+        await afpNews.authenticate({ username: 'TEST', password: 'TEST' });
+      } catch (e) {
+        expect(e).to.be.an('error');
+      }
+    });
+    it('should return an authenticated token when called with api key and credentials', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      const token = await afpNews.authenticate({ username, password });
+      expect(token.accessToken).to.be.a('string');
+      expect(token.refreshToken).to.be.a('string');
+      expect(token.tokenExpires).to.be.a('number');
+      expect(token).to.deep.equal(afpNews.token);
+    });
+    it('should refresh token when token expires', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      const token = await afpNews.authenticate({ username, password });
+      afpNews._tokenExpires = 0;
+      const newToken = await afpNews.authenticate();
+      expect(token.accessToken).to.not.be.equal(newToken.accessToken);
+    });
+    it('should not refresh token when token is valid', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      const token = await afpNews.authenticate({ username, password });
+      const newToken = await afpNews.authenticate();
+      expect(token.accessToken).to.be.equal(newToken.accessToken);
+    });
+  });
+  describe('Search', async function() {
+    it('should return the search url', function() {
+      const afpNews = new AfpNews();
+      expect(afpNews.searchUrl).to.be.a('string');
+    });
+    it('should return the default search params', function() {
+      const afpNews = new AfpNews();
+      expect(afpNews.defaultSearchParams).to.have.all.keys('lang', 'urgency', 'searchTerms', 'size', 'dateFrom', 'dateTo', 'sort');
+    });
+    it('should return a news array with anonymous token, without explicit call to authenticate', async function() {
+      const afpNews = new AfpNews();
+      const news = await afpNews.search();
+      expect(news.documents).to.be.an('array');
+      expect(news.count).to.be.a('number');
+    });
+    it('should return a news array with authenticated token', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      await afpNews.authenticate({ username, password });
+      const news = await afpNews.search();
+      expect(news.documents).to.have.lengthOf.within(1, afpNews.defaultSearchParams.size);
+      expect(news.count).to.be.at.least(news.documents.length);
+    });
+    it('should react to custom params', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      await afpNews.authenticate({ username, password });
+      const customParams = {
+        size: 15,
+        dateFrom: 'now-1M',
+        dateTo: 'now-1d',
+        lang: 'fr',
+        urgency: 3,
+        sort: 'published asc'
+      }
+      const news = await afpNews.search(customParams);
+      expect(news.documents).to.have.lengthOf.within(1, customParams.size);
+      expect(news.count).to.be.at.least(news.documents.length);
+      const firstDocument = news.documents[0];
+      expect(firstDocument).to.be.an('object').that.includes.all.keys('country', 'city', 'lang', 'title', 'urgency', 'href', 'headline', 'slug', 'news', 'product', 'created', 'published', 'uno');
+      expect(firstDocument.lang).to.be.equal(customParams.lang);
+      expect(firstDocument.urgency).to.be.equal(customParams.urgency);
+      const lastDocument = news.documents[news.documents.length - 1];
+      expect(new Date(firstDocument.published)).to.be.below(new Date(lastDocument.published));
+      expect(new Date(firstDocument.published)).to.be.below(new Date(Date.now() - 2419200)); // now-1M
+      expect(new Date(lastDocument.published)).to.be.below(new Date(Date.now() - 86400)); // now-1d
+    });
+    it('should react to search terms string', async function() {
+      const afpNews = new AfpNews({ apiKey });
+      await afpNews.authenticate({ username, password });
+      const news = await afpNews.search({ searchTerms: 'title:France Allemagne', dateFrom: 'now-12M' });
+      const randomDocument = news.documents.find(d => {
+        return d.headline && Array.isArray(d.news)
+      });
+      expect(randomDocument.headline).to.have.string('France');
+      const text = randomDocument.news.reduce((acc, current) => {
+        return `${acc} ${current}`;
+      }, '');
+      expect(text).to.have.string('Allemagne');
     });
   });
 });
