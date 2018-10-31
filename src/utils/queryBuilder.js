63cb4a256642b1398734d36877fa12d822cba844
Jules Bonnard <julesbonnard@gmail.com>
2018-10-31 20:31:04 +0100

    refactor queryBuilder

--- src/utils/queryBuilder.js
+++ src/utils/queryBuilder.js
@@ -2,58 +2,59 @@ import { parse as queryParser } from 'lucene-query-parser'
 import { normalize } from './normalizer'
 
 function recursiveBuild (query) {
-  if (query.operator === '<implicit>') {
-    return [{
-      and: [
-        ...recursiveBuild(query.left),
-        ...recursiveBuild(query.right)
-      ]
-    }]
-  } else if (query.operator === 'AND') {
-    return [{
-      and: [
-        ...recursiveBuild(query.left),
-        ...recursiveBuild(query.right)
-      ]
-    }]
-  } else if (query.operator === 'OR') {
-    return [{
-      or: [
-        ...recursiveBuild(query.left),
-        ...recursiveBuild(query.right)
-      ]
-    }]
-  } else if (!query.operator) {
-    if (query.left) {
-      return [
-        ...recursiveBuild(query.left)
-      ]
-    }
-    if (query.field === '<implicit>') {
+  switch (query.operator) {
+    case '<implicit>':
       return [{
-        [query.prefix === '-' ? 'and' : 'or']: [
-          ...recursiveBuild(Object.assign({}, query, { field: 'news' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'slug' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'city' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'country' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'title' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'caption' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'creator' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'headline' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'entity_person' })),
-          ...recursiveBuild(Object.assign({}, query, { field: 'entity_location' }))
+        and: [
+          ...recursiveBuild(query.left),
+          ...recursiveBuild(query.right)
         ]
       }]
-    }
-    const object = {
-      name: normalize(query.field)
-    }
-    if (query.prefix === '-') {
-      object['exclude'] = [normalize(query.term)]
-    } else {
-      object['in'] = [normalize(query.term)]
-    }
-    return [object]
+    case 'AND':
+      return [{
+        and: [
+          ...recursiveBuild(query.left),
+          ...recursiveBuild(query.right)
+        ]
+      }]
+    case 'OR':
+      return [{
+        or: [
+          ...recursiveBuild(query.left),
+          ...recursiveBuild(query.right)
+        ]
+      }]
+    default:
+      if (query.left) {
+        return [
+          ...recursiveBuild(query.left)
+        ]
+      }
+      if (query.field === '<implicit>') {
+        return [{
+          [query.prefix === '-' ? 'and' : 'or']: [
+            ...recursiveBuild({ ...query, field: 'news' }),
+            ...recursiveBuild({ ...query, field: 'slug' }),
+            ...recursiveBuild({ ...query, field: 'city' }),
+            ...recursiveBuild({ ...query, field: 'country' }),
+            ...recursiveBuild({ ...query, field: 'title' }),
+            ...recursiveBuild({ ...query, field: 'caption' }),
+            ...recursiveBuild({ ...query, field: 'creator' }),
+            ...recursiveBuild({ ...query, field: 'headline' }),
+            ...recursiveBuild({ ...query, field: 'entity_person' }),
+            ...recursiveBuild({ ...query, field: 'entity_location' })
+          ]
+        }]
+      }
+      const object = {
+        name: normalize(query.field)
+      }
+      if (query.prefix === '-') {
+        object['exclude'] = [normalize(query.term)]
+      } else {
+        object['in'] = [normalize(query.term)]
+      }
+      return [object]
   }
 }
 
